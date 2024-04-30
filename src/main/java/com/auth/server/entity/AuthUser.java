6e3f6d7997376dde4bef87f71b7c796c457ea0d8
daoninhthai <thaimeo1131@gmail.com>
2024-04-30 16:42:19 +0700

    style: fix indentation and formatting

--- src/main/java/com/auth/server/entity/AuthUser.java
+++ src/main/java/com/auth/server/entity/AuthUser.java
@@ -24,6 +24,7 @@ public class AuthUser {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+
     @Column(nullable = false, unique = true, length = 50)
     private String username;
 
